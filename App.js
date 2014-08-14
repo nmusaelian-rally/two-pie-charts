16a1059b6b2e7685f7495c098d5af21cea3ea626
N M <nmusaelian@rallydev.com>
2014-08-14 09:16:27 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,101 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    _atTheStartAddedChart: null,
+    _stateChart: null,
+    
+    launch: function() {
+        this._createAtTheStartAddedChart();
+        this._createStateChart();
+    },
+
+    _createAtTheStartAddedChart: function () {
+
+    var series = [
+        {
+            type: 'pie',
+            name: 'Features',
+            data: [
+                {
+                    name: 'At the Start',
+                    y: 20,
+                    color: '#0CDBE8'
+                },
+                {
+                    name: 'Added During Release',
+                    y: 30,
+                    color: '#FFE11A'
+                }
+            ]
+        }
+    ];
+    this._createChart(series);
+},
+
+    _createStateChart: function () {
+        var me = this;
+    
+        var series = [
+            {
+                type: 'pie',
+                name: 'Features',
+                data: [
+                    {
+                        name: 'Not Completed in Time',
+                        y: 10,
+                        color: '#FFE11A'
+                    },
+                    {
+                        name: 'Completed in Time',
+                        y: 15,
+                        color: '#98C000'
+                    },
+                    {
+                        name: 'Removed from Release',
+                        y: 20,
+                        color: '#EA2E49'
+                    },
+                    {
+                        name: 'Completely Removed',
+                        y: 5,
+                        color: '#3D4C53'
+                    }
+                ]
+            }
+        ];
+        this._createChart(series);
+},
+
+    _createChart: function (series) {
+        var chart = Ext.create('Rally.ui.chart.Chart', {
+            chartConfig: {
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false
+                },
+                title: {
+                    text: 'Release Features'
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b>{point.y}</b>'
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: false,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: false
+                        },
+                        showInLegend: true
+                    }
+                }
+            },
+            chartData: {
+                series: series
+            }
+        });
+        this.add(chart);
+    }
+
+});
---
+++ README.md
@@ -0,0 +1,13 @@
+Chart Example
+=========================
+
+## Overview
+![](pic.png)
+
+## License
+This app is available AS IS. It is not supported by Rally support.
+AppTemplate is released under the MIT license.  See the file [LICENSE](./LICENSE) for the full text.
+
+##Documentation for SDK
+
+You can find the documentation on our help [site.](https://help.rallydev.com/apps/2.0rc3/doc/)
